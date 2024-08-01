74a55c5ef5f4992e1224f5dead4d34ea6d6a0f0b
jignaciodegiovanni <jignaciodegiovanni@gmail.com>
2024-08-01 04:30:25 -0300

    Updated gitignore

--- obj/Debug/net6.0/MyPluginInfo.cs
+++
@@ -1,9 +0,0 @@
-namespace SODCustomStateInjectorMiami
-{
-    public static class MyPluginInfo
-    {
-        public const string PLUGIN_GUID = "SOD.CustomStateInjector.Miami";
-        public const string PLUGIN_NAME = "SOD CustomStateInjector Miami";
-        public const string PLUGIN_VERSION = "1.0.0";
-    }
-}
